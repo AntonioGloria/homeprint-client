936694dbf9cbabba65d3edff538352cb7a9e9455
Antonio Gloria <angloria87@gmail.com>
2023-09-22 10:48:09 -0600

    Added ability to choose new file and validations for inputs

--- src/components/PrintJobForm.jsx
+++ src/components/PrintJobForm.jsx
@@ -6,6 +6,7 @@ const PrintJobForm = () => {
 
   const [preview, setPreview] = useState(null)
   const [message, setMessage] = useState(null)
+  const [msgClass, setMsgClass] = useState('')
   const [file, setFile] = useState(null)
   const [pages, setPages] = useState('All')
   const [copies, setCopies] = useState(1)
@@ -25,23 +26,38 @@ const PrintJobForm = () => {
       const formData = new FormData()
       formData.append("file", addedFile)
       const upload = await axios.post(`${serverURL}/upload`, formData)
-      setFile(upload.data);
+      setFile(upload.data)
+      setMsgClass('')
+      setMessage(null)
     }
 
     catch (error) {
-      setMessage(error)
+      setMsgClass('errorMsg')
+      setMessage(`ERROR: ${error.message}`)
     }
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
+      if(!file) {
+        throw new Error("You must add a file!")
+      }
+
+      if (!pages) {
+        throw new Error(`You must fill in the Pages field!`)
+      }
+
       const options = { pages, copies, monochrome, scale }
       const response = await axios.post(`${serverURL}/print`, { file, options })
-      setMessage(response.data);
+
+      setMsgClass('')
+      setMessage(response.data)
     }
+
     catch (error) {
-      setMessage(error)
+      setMsgClass('errorMsg')
+      setMessage(`ERROR: ${error.message}`)
     }
   }
 
@@ -49,28 +65,37 @@ const PrintJobForm = () => {
     <form onSubmit={handleSubmit}>
       <h2>Print A PDF File</h2>
       <div className='form-main'>
-        <label htmlFor='print-file' className='form-section' onChange={(e) => handleAddFile(e)}>
-          <object data={preview} className='pdf-viewer' type='application/pdf'>
-            <div className='file-preview'>
-              <h2>Click here to add a file to print</h2>
-            </div>
-          </object>
-          <input id='print-file' type='file' accept='application/pdf' hidden/>
-        </label>
+
+        {!(preview && file) &&
+          <label htmlFor='print-file' className='file-placeholder' onChange={(e) => handleAddFile(e)}>
+            <h2>Click here to add a file to print</h2>
+            <input id='print-file' type='file' accept='application/pdf' hidden/>
+          </label>
+        }
+
+        {(preview && file) &&
+          <div className='file-preview'>
+            <iframe src={preview}/>
+            <label htmlFor='print-file' onChange={(e) => handleAddFile(e)}>
+              Choose a different file
+              <input id='print-file' type='file' accept='application/pdf' hidden/>
+            </label>
+          </div>
+        }
 
         <div className='form-section'>
           <div className='input-set'>
             <label htmlFor='pages'><strong>Page(s) to Print: </strong></label>
-            <input name='pages' type='text' value={pages} onChange={(e) => setPages(e.target.value)}/>
+            <input id='pages' name='pages' type='text' value={pages} onChange={(e) => setPages(e.target.value)}/>
           </div>
 
           <div className='input-set'>
             <label htmlFor='copies'><strong>Copies: </strong></label>
-            <input name='copies' type='number' min={1} value={copies} onChange={(e) => setCopies(e.target.value)}/>
+            <input id='copies' name='copies' type='number' min={1} value={copies} onChange={(e) => setCopies(e.target.value)}/>
           </div>
 
           <div className='input-set'>
-          <label htmlFor='monochrome'><strong>Color Options</strong></label>
+          <strong>Color Options</strong>
             <div className='radio-grp'>
               <label>
                 Black and White
@@ -84,7 +109,7 @@ const PrintJobForm = () => {
           </div>
 
           <div className='input-set'>
-            <label htmlFor='scale'><strong>Document Scale</strong></label>
+            <strong>Document Scale</strong>
             <div className='radio-grp'>
               <label>
                 Fit
@@ -105,7 +130,11 @@ const PrintJobForm = () => {
 
       <div className='form-end'>
         <button type='submit'>Print File</button>
-        {message && <p>{message}</p>}
+        {message &&
+          <p className={msgClass}>
+            <strong>{`[${message}]`}</strong>
+          </p>
+        }
       </div>
 
     </form>
